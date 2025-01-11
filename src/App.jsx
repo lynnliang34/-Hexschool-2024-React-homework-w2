bb418c8b20e540742f749828e79be28d7399c461
lynnliang34 <lynnliang34@gmail.com>
2025-01-11 14:07:47 +0800

    feat: 三元運算子切換頁面

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useState } from "react";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
   const [account, setAccount] = useState({
     username: "",
     password: "",
@@ -20,42 +22,48 @@ function App() {
     e.preventDefault();
     axios
       .post(`${import.meta.env.VITE_BASE_URL}/admin/signin`, account)
-      .then((res) => console.log(res))
+      .then((res) => setIsLogin(true))
       .catch((error) => alert("登入失敗"));
   };
 
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-      <h1 className="mb-5">請先登入</h1>
-      <form onSubmit={handleLogin} className="d-flex flex-column gap-3">
-        <div className="form-floating mb-3">
-          <input
-            name="username"
-            value={account.username}
-            onChange={handleInputChange}
-            type="email"
-            className="form-control"
-            id="username"
-            placeholder="name@example.com"
-          />
-          <label htmlFor="username">Email address</label>
+    <>
+      {isLogin ? (
+        <p>已登入</p>
+      ) : (
+        <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+          <h1 className="mb-5">請先登入</h1>
+          <form onSubmit={handleLogin} className="d-flex flex-column gap-3">
+            <div className="form-floating mb-3">
+              <input
+                name="username"
+                value={account.username}
+                onChange={handleInputChange}
+                type="email"
+                className="form-control"
+                id="username"
+                placeholder="name@example.com"
+              />
+              <label htmlFor="username">Email address</label>
+            </div>
+            <div className="form-floating">
+              <input
+                name="password"
+                value={account.password}
+                onChange={handleInputChange}
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+            <button className="btn btn-primary">登入</button>
+          </form>
+          <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
         </div>
-        <div className="form-floating">
-          <input
-            name="password"
-            value={account.password}
-            onChange={handleInputChange}
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Password"
-          />
-          <label htmlFor="password">Password</label>
-        </div>
-        <button className="btn btn-primary">登入</button>
-      </form>
-      <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
-    </div>
+      )}
+    </>
   );
 }
 
